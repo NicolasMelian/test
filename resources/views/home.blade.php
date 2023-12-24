a97b0179151ac6e41fc7f897de5cb13e6fbacacc
Nico Melian <nicolasgmelian@gmail.com>
2023-12-24 05:57:08 -0300

    favicon

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-    <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.png') }}">
+    <img rel="icon" type="image/x-icon" href="images/favicon.png">
 
 
 

0b9b5737ec54fa7c5dc3b05708dc1221fa710e3c
Nico Melian <nicolasgmelian@gmail.com>
2023-12-24 05:52:53 -0300

    edicion script

--- scripts/ocr_script.py
+++ scripts/ocr_script.py
@@ -4,17 +4,15 @@ import pytesseract
 from pytesseract import image_to_string
 
 # Configurar la ruta del ejecutable de Tesseract si no está en tu PATH
-pytesseract.pytesseract.tesseract_cmd = '/opt/homebrew/bin/tesseract'
-
+pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
 
 def preprocess_image(image_path):
     img = Image.open(image_path)
-    img = img.convert('L') # Convertir a escala de grises
+    img = img.convert('L')  # Convertir a escala de grises
     enhancer = ImageEnhance.Contrast(img)
-    img = enhancer.enhance(2) # Aumentar el contraste
-    img = img.filter(ImageFilter.MedianFilter()) # Aplicar filtro de mediana
+    img = enhancer.enhance(2)  # Aumentar el contraste
+    img = img.filter(ImageFilter.MedianFilter())  # Aplicar filtro de mediana
     return img
-
 
 def extract_text(img):
     try:

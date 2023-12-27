a83952398930de7150d79cb62bd72203137d8f21
Nico Melian <nicolasgmelian@gmail.com>
2023-12-27 13:22:20 -0300

    alt en imagenes

--- resources/views/components/explication.blade.php
+++ resources/views/components/explication.blade.php
@@ -7,7 +7,7 @@
         <p>Ya sea arrastrando y soltado o mediante el boton de "Elegir Archivos" sube tu imagen o imagenes a nuestro formulario. Acepta cualquier tipo de imágen o fotos, en formatos: jpg, png y más.</p>
       </div>
       <div class="w-full md:w-1/2 px-4">
-        <img src="/images/imagen-descripcion-1.png" alt="Paso 1 explicando como convertir imagenes a texto" class="w-full max-w-md mx-auto" style="max-width: 420px;">
+        <img src="/images/imagen1-descripcion.png" alt="Paso 1 explicando como convertir imagenes a texto" class="w-full max-w-md mx-auto" style="width:700px, height:300px">
       </div>
     </div>
   
@@ -18,7 +18,7 @@
         <p>Luego de subir tus imagenes, apreta en el boton "Extraer Texto", tus imagenes son enviadas a nuestro servidor que mediante Tesseract OCR extrae el texto de ellas y te lo devuelve debajo del formulario. Los textos pueden ser desde oraciones a largos parrafos.</p>
       </div>
       <div class="w-full md:w-1/2 px-4">
-        <img src="/images/imagen-descripcion-2.png" class="w-full max-w-md mx-auto" style="max-width: 420px;" alt="Paso 2 explicando el conversor de imagen a texto">
+        <img src="/images/imagen2-descripcion.png" class="w-full max-w-md mx-auto" alt="Paso 2 explicando el conversor de imagen a texto" style="width:700px, height:300px">
       </div>
     </div>
   
@@ -29,7 +29,7 @@
         <p>Una vez apretas "Extraer Texto" el texto de tus imagenes se mostrará debajo donde vas a poder copiar o descargarlo en formato .txt, tambien tienes la posibilidad de editar antes de copiar o descargarlo en caso de que haya un pequeño error en alguna letra o palabra. Cuando nuestra aplicacion extrae el texto de tus imagenes, estas son borradas automáticamente para garantizar la máxima seguridad a nuestros usuarios.</p>
       </div>
       <div class="w-full md:w-1/2 px-4">
-        <img src="/images/imagen-descripcion-3.png" class="w-full max-w-md mx-auto" style="max-width: 420px;" alt="Paso 3 de extraer texto de imagen">
+        <img src="/images/imagen3-descripcion.png" class="w-full max-w-md mx-auto" alt="Paso 3 de extraer texto de imagen" style="width:700px, height:300px">
       </div>
     </div>
   
@@ -40,7 +40,7 @@
         <p>Si quieres extraer el texto de más imagenes, haz click en empezar de nuevo, nuestro formulario se limpia y puedes subiar más imagenes o fotos.</p>
       </div>
       <div class="w-full md:w-1/2 px-4">
-        <img src="/images/imagen-descripcion-4.png" class="w-full max-w-md mx-auto" style="max-width: 420px;" alt="Paso 4 para sacar texto de imagen">
+        <img src="/images/imagen4-descripcion.png" class="w-full max-w-md mx-auto" alt="Paso 4 para sacar texto de imagen"  style="width:700px, height:300px">
       </div>
     </div>
   
